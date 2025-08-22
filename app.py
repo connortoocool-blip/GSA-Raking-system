f806b9c2510c5a24484c88c2505526a6b148173c
connortoocool-blip <connortoocool@gmail.com>
2025-08-22 11:08:42 +1000

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="GSA UTR", layout="centered")
+
+st.title("🎾 GSA Exclusive UTR System")
+st.write("Own it. Rise it. Go beyond.")
+st.success("Welcome to the GSA Beyond UTR Ranking App!")
